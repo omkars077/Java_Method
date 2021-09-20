e313b74d3cd05f50d704a2d842d99ae1bfddcb06
Omkar Singh <omkars077@gmail.com>
2021-09-19 22:02:29 -0700

    calcy with return

--- src/nextchapter/Calculator.java
+++ src/nextchapter/Calculator.java
@@ -2,20 +2,14 @@ package nextchapter;
 
 public class Calculator {
 
-
-    public void addition(int a , int b){
-        int result=a+b;
-        System.out.println("The addition of two value will be : "+result);
-    }
-    public void multi(int a, int b){
-        int output=a*b;
-        System.out.println("The multi of two value will be : "+output);
+    public int  addition(int a , int b){
+        int output=a+b;
+        return output;
     }
     public static void main(String[] args){
         Calculator obj= new Calculator();
-        obj.addition(6859,4593);
-        obj.multi(345,89);
-
+       int var= obj.addition(786,897);
+        System.out.println("The addition of two value will be : "+var);
     }
 
 
--- src/nextchapter/NewFunction.java
+++ src/nextchapter/NewFunction.java
@@ -2,18 +2,7 @@ package nextchapter;
 
 public class NewFunction {
 
-    public void message(String brand , double price){
-        System.out.println("This is the latest version : "+brand);
-        System.out.println("The price of this brand is : "+price+" rs ");
-    }
-    public void student(String name, int age){
-        System.out.println("His real name is : "+name+" : "+"And his age is : "+age);
-    }
-    public static void main(String[] args){
-        NewFunction obj= new NewFunction();
-        obj.message("Samsung",12000.5);
-        obj.student("Rohit Sharma",32);
-    }
+
 
 
 }
