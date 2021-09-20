24d4d5a8d76c4c93d1c59f326811c94f1bb0f003
Omkar Singh <omkars077@gmail.com>
2021-09-19 21:26:48 -0700

    calcy

--- src/nextchapter/Calculator.java
+++ src/nextchapter/Calculator.java
@@ -2,28 +2,22 @@ package nextchapter;
 
 public class Calculator {
 
-    public void addition(){
-        int a=234;
-        int b=987;
-        int output=a+b;
-        System.out.println("The addition of two value will be : "+output);
+
+    public void addition(int a , int b){
+        int result=a+b;
+        System.out.println("The addition of two value will be : "+result);
     }
-    public void multi(){
-        int a=89;
-        int b=45;
-        int result=a*b;
-        System.out.println("The multiply of two value will be : "+result);
-    }
-    public void divide(){
-        int a=625;
-        int b=5;
-        int value=a/b;
-        System.out.println("This is divide method : "+value);
+    public void multi(int a, int b){
+        int output=a*b;
+        System.out.println("The multi of two value will be : "+output);
     }
     public static void main(String[] args){
         Calculator obj= new Calculator();
-        obj.addition();
-        obj.multi();
-        obj.divide();
+        obj.addition(6859,4593);
+        obj.multi(345,89);
+
     }
+
+
+
 }
